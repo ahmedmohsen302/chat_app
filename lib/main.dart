6ebb5540e35738dc347a57d53d44ba916dce2bd9
ahmedmohsen302 <ahmedthemohsen@gmail.com>
2025-04-08 06:48:45 +0200

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:chat_app/views/login_view.dart';
 import 'package:chat_app/views/sign_up_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const chatApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const ChatApp());
 }
 
-class chatApp extends StatelessWidget {
-  const chatApp({super.key});
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
 
   @override
   Widget build(BuildContext context) {
